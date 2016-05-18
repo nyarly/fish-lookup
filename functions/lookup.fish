50da424ff11f2b308fdc86cbb5a785c4455d0b88
Judson <nyarly@gmail.com>
2016-05-18 10:45:49 -0700

    Created

---
+++ functions/lookup.fish
@@ -0,0 +1,18 @@
+function lookup --argument file
+	if [ (count $argv) -ne 1 ]
+    echo "Usage: $0 <file>"
+    return 1
+  end
+
+  pushd .
+  while [ (pwd) != '/' ]
+    if [ -e $file ]
+      pwd
+      popd
+      return 0
+    end
+    cd ..
+  end
+  popd
+  return 1
+end
